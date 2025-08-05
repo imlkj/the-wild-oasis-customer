7cb5c879defc4959343e3a2105b2a408d2a51926
Lalit Jaiswal <83780467+imlkj@users.noreply.github.com>
2025-08-05 23:27:31 +0545

    Set up home page with link to cabins

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Page() {
-  return <h1>Hello Next!</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paradise</h1>
+      <h3>
+        <Link href="/cabins">Explore luxury cabins</Link>
+      </h3>
+    </div>
+  );
 }
