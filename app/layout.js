1deaff1cb85896df2b4a29d4a16cc178423346b8
Lalit Jaiswal <83780467+imlkj@users.noreply.github.com>
2025-08-06 08:17:47 +0545

    refactor: update layout with new Header component and Josefin Sans font

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,31 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="mx-auto max-w-7xl w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
