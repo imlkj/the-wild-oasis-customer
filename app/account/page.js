417507f0dba28f7b61f3ec1b93214edc65ccad35
Lalit Jaiswal <83780467+imlkj@users.noreply.github.com>
2025-10-08 12:51:55 +0545

    feat(auth): implement Google sign-in and login page integration
    - Added `signInAction` server action to handle Google authentication and redirect to /account
    - Updated `SignInButton` to use form-based sign-in via server action
    - Extended `auth.js` to export `signIn`, `signOut`, and define custom signIn page route
    - Created new `/login` page with Google sign-in button
    - Enhanced `/account` page to greet logged-in user by first name

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-function Page() {
-  return <h1>Accout Section</h1>;
+async function Page() {
+  const session = await auth();
+  return (
+    <h1 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {(session?.user?.name).split(" ")[0]} 🙏🏻
+    </h1>
+  );
 }
 
 export default Page;
