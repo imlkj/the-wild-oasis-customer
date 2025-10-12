6d4525baf6f01e4c70fc27e43de1fe09ea9ffe59
Lalit Jaiswal <83780467+imlkj@users.noreply.github.com>
2025-10-12 23:55:00 +0545

    feat(reservation): add update reservation page with form handling and submit button

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -79,6 +80,46 @@ export async function deleteReservation(bookingId) {
   }
 
   revalidatePath("/account/reservations");
+}
+
+export async function updateBooking(formData) {
+  const session = await auth();
+  if (!session) {
+    throw new Error("you must be logged in");
+  }
+  const bookingId = Number(formData.get("bookingId"));
+
+  const guestBookings = await getBookings(session.user.guestId);
+
+  const guestBookingsIds = guestBookings.map((booking) => {
+    return booking.id;
+  });
+
+  if (!guestBookingsIds.includes(bookingId)) {
+    throw new Error("You are not alowed to update this booking");
+  }
+
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+  //Revalidation should happen before redirecing
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  redirect("/account/reservations");
 }
 
 export async function signInAction() {
